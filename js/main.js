e96bd8905585a98ca634482e2bfa54f165eaadff
martyna1308 <martyna1308@gmail.com>
2019-09-12 22:22:00 +0200

    2

--- js/main.js
+++ js/main.js
@@ -5,10 +5,7 @@ let albumsOffset = document.getElementById("albums").offsetTop;
 let square = document.querySelector(".albums-items .square");
 
 window.addEventListener( "scroll", () => {
-    if (this.scrollY > (albumsOffset-300)) {
-        square.style.animation = "rotateMe";
-    }
-    
+
 
 })
 
